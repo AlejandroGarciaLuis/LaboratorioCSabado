e99d316844bc70ccb4d926cd75e2e6b3f0941498
Alejandro Garcia <alexgarcialuis@hotmail.com>
2019-08-25 14:47:29 -0500

    Commit para Practica3

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	return 0;
+int main(){
+	float x;
+	//estas variables son para identificar el resultado de cada ecuación//
+	float a,b,c,d;
+	
+	//aquí pido el numero que se evaluará en cada una de las ecucaciones//
+	printf("Ingrese el valor a evaluar \n");
+	scanf("%f", &x);
+	
+	//ecuación 1//
+	a=((x-1)/4)-((x-5)/36);
+	printf("El resultado para x=1 es %f",a);
+	
+	//ecuación 2//
+	b=(((x+1)/8)-((2*x-3)/16))*6;
+	printf("El resultado para x=1 es %f",b);
 }
