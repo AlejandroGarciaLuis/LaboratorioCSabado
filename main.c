352d82a9c802bbe22106b457d279c7603b4fe0b3
Alejandro Garcia <alexgarcialuis@hotmail.com>
2019-08-25 14:49:13 -0500

    Commit para Practica3

--- main.c
+++ main.c
@@ -10,9 +10,13 @@ int main(){
 	
 	//ecuación 1//
 	a=((x-1)/4)-((x-5)/36);
-	printf("El resultado para x=1 es %f",a);
+	printf("El resultado es %f",a);
 	
 	//ecuación 2//
 	b=(((x+1)/8)-((2*x-3)/16))*6;
-	printf("El resultado para x=1 es %f",b);
+	printf("El resultado es %f",b);
+	
+	//ecuación 3//
+	c=((x-((x-2)/3))*(2/3))+1;
+	printf("El resultado es %f",c);
 }
