f7c0cce5e1269e88da0e8846b4e295164a8fe822
Alejandro Garcia <alexgarcialuis@hotmail.com>
2019-10-12 10:53:02 -0500

    Practica 10

--- main.c
+++ main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main(){
+	/* Alejandro García Luis
+	   12/10/2019
+	   Grupo 008
+	   Sábados 9am-11am
+	   1846616 */
+	setlocale(LC_ALL,"spanish");
+	char pal[40], pal2[40];
+	int n,i,x,a=0,p=0,e=0;
+	char b=' ';
+	
+	printf("Ingrese una frase: \n");
+	gets(pal);
+	n=strlen(pal); //cuenta el numero de caracteres//
+	
+	for(i=0; i<=n; i++){
+		if(pal[i]==b)
+			i++;
+			pal2[e]=pal[i];
+			e++;
+		for(x=n-1; x>=0; x--){
+		printf("\n %c \t",pal[x]);
+		x=0;
+		n=strlen(pal2);
+		}
+	}
+	
+    
 
-int main(int argc, char *argv[]) {
-	return 0;
+return 0;
 }
