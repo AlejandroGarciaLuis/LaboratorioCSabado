a65d917c6d6e717330deabe5d1edca2d89e464cf
Alejandro Garcia Luis <alexgarcialuis@hotmail.com>
2019-11-16 10:58:31 -0600

    Practica 15

--- main.c
+++ main.c
@@ -1,8 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	return 0;
+#include <locale.h>
+/*Alejandro García Luis
+16/11/20219
+Grupo 008
+Sabados 9-11am
+1846616 */
+int main(){
+	//******ACTIVIDAD 1******//
+	setlocale(LC_ALL,"spanish");
+	char ifilename[]="practica.txt";
+	char ofilename[]="archivo_alreves.txt";
+	char cadena[20];
+	char nuevacad[20];
+	char pro[10]="procesado";
+	int i,j;
+	
+	FILE *ifp, *ofp;
+	ifp=fopen(ifilename,"r");
+	
+	while (fgets(cadena, sizeof cadena, ifp) != NULL) {
+    
+	printf("\n %s \n",cadena);
+}
+    ofp=fopen(ofilename,"w");
+    while(cadena[i]!='\0'){
+        i++;
+    }
+    fprintf(ofp,"La cadena al revés es:\n\t");
+    for (j=i-1; j>=0; j--){
+        fprintf(ofp,"%c",cadena[j]);
+    }
+    fclose(ofp);
+    fclose(ifp);
+    
+    ifp=fopen(ifilename,"w");
+    fprintf(ifp,"%s",pro);
+    fclose(ifp);
+    
+    //******ACTIVIDAD 2******//
+    char afilename[]="numeros.txt";
+    char efilename[]="suma.txt";
+    int suma, a, b;
+    
+    FILE *afp, *efp;
+    afp=fopen(afilename,"r");
+    fscanf(afp,"%d %d",&a,&b);
+    efp=fopen(efilename,"w");
+    fprintf(efp,"la suma de los dos números es %d",a+b);
+    fclose(efp);
+    fclose(afp);
 }
